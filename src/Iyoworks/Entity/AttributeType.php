fa2916df0019bc2e7c477ed2c069ebeb601f81e7
Blessing Osakue <iyoworks@gmail.com>
2013-12-20 18:19:29 -0600

    add docs and refactored the way classes are instantiated

--- src/Iyoworks/Entity/AttributeType.php
+++ src/Iyoworks/Entity/AttributeType.php
@@ -6,20 +6,29 @@ use Iyoworks\Support\Str;
 
 class AttributeType extends AttributeEnum {
     const DEFAULT_COLLECTION_CLASS = 'Illuminate\Support\Collection';
+    const DEFAULT_ENTITY_CLASS = 'stdClass';
+    /**
+     * @var bool
+     */
     protected static $booted = false;
+    /**
+     * @var array
+     */
     protected $baseDefinition = [
         'guarded' => null,
         'visible' => true,
         'type' => AttributeType::Mixed
     ];
-
+    /**
+     * @var array
+     */
     static $defaultDefinitions = [
         AttributeType::PK => [
             'guarded' => true
         ],
         AttributeType::Entity => [
             'key' => null,
-            'class' => null,
+            'class' => self::DEFAULT_ENTITY_CLASS,
             'many' => false,
             'pivot_data' => [],
             'collection' => self::DEFAULT_COLLECTION_CLASS,
@@ -39,7 +48,7 @@ class AttributeType extends AttributeEnum {
         ]
     ];
 
-    function __construct()
+    public function __construct()
     {
         if (!static::$booted)
         {
@@ -50,6 +59,11 @@ class AttributeType extends AttributeEnum {
 
     protected static function boot() { }
 
+    /**
+     * @param $type
+     * @param $value
+     * @return mixed
+     */
     public function set($type, $value)
     {
         $def = $this->getFullDefinition($type);
@@ -60,6 +74,11 @@ class AttributeType extends AttributeEnum {
         return $value;
     }
 
+    /**
+     * @param $type
+     * @param $value
+     * @return mixed
+     */
     public function get($type, $value)
     {
         $def = $this->getFullDefinition($type);
@@ -70,6 +89,10 @@ class AttributeType extends AttributeEnum {
         return $value;
     }
 
+    /**
+     * @param $type
+     * @return bool
+     */
     public function isValidType($type)
     {
         return array_key_exists($type, $this->toArray()) ||
@@ -86,42 +109,80 @@ class AttributeType extends AttributeEnum {
         return in_array($type, [static::DateTime, static::Timestamp]);
     }
 
+    /**
+     * @return array
+     */
     public function toArray(){
         static $reflection;
         if(is_null($reflection)) $reflection = new \ReflectionClass($this);
         return $reflection->getConstants();
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return int
+     */
     public function setInteger($value, array $def)
     {
         return (int) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return int
+     */
     public function getInteger($value, array $def)
     {
         return (int) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return bool
+     */
     public function setBoolean($value, array $def)
     {
         return (bool) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return bool
+     */
     public function getBoolean($value, array $def)
     {
         return (bool) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return float
+     */
     public function setDouble($value, array $def)
     {
         return (double) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return float
+     */
     public function getDouble($value, array $def)
     {
         return (double) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return object
+     */
     public function setEntity($value, array $def)
     {
         $class = $def['class'];
@@ -143,32 +204,56 @@ class AttributeType extends AttributeEnum {
         return $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return object
+     */
     public function getEntity($value, array $def)
     {
         if(is_null($value))
         {
             if ($def['many']) return $this->makeCollection($def['collection']);
-            $class = $def['class'] ?: 'stdClass';
-            return new $class;
+            return $this->makeClass($def['class']);
         }
         return $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return float
+     */
     public function setFloat($value, array $def)
     {
         return (float) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return float
+     */
     public function getFloat($value, array $def)
     {
         return (float) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return mixed
+     */
     public function setHandle($value, array $def)
     {
         return handle($value);
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return string
+     */
     public function setJson($value, array $def)
     {
         if($value instanceof \Illuminate\Support\Contracts\JsonableInterface)
@@ -180,6 +265,11 @@ class AttributeType extends AttributeEnum {
         return $output;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return array|mixed
+     */
     public function getJson($value, array $def)
     {
         if (empty($value) or $value == 'null')
@@ -190,41 +280,81 @@ class AttributeType extends AttributeEnum {
         return $output;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return int
+     */
     public function setPrimaryKey($value, array $def)
     {
         return  (int) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return mixed
+     */
     public function getPrimaryKey($value, array $def)
     {
         return $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return string
+     */
     public function setSerial($value, array $def)
     {
         return  serialize($value);
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return mixed
+     */
     public function getSerial($value, array $def)
     {
         return unserialize($value);
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return mixed
+     */
     public function setSlug($value, array $def)
     {
         return slugify($value);
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return string
+     */
     public function setString($value, array $def)
     {
         return (string) $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return string
+     */
     public function getString($value, array $def)
     {
         return (string)  $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return string
+     */
     public function setTimestamp($value, array $def)
     {
         if(is_string($value))
@@ -235,6 +365,11 @@ class AttributeType extends AttributeEnum {
         return $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return DateTime
+     */
     public function getTimestamp($value, array $def)
     {
         if (is_null($value)) return $this->newDateObject();
@@ -255,6 +390,11 @@ class AttributeType extends AttributeEnum {
         return $this->newDateFromFormat($value, $def['format']);
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return mixed
+     */
     public function getUid($value, array $def)
     {
         if($def['auto'] and is_null($value))
@@ -262,6 +402,11 @@ class AttributeType extends AttributeEnum {
         return $value;
     }
 
+    /**
+     * @param $value
+     * @param array $def
+     * @return mixed
+     */
     public function setUid($value, array $def)
     {
         return $value;
@@ -305,17 +450,30 @@ class AttributeType extends AttributeEnum {
         return $this->baseDefinition;
     }
 
+    /**
+     * @param $class
+     * @param $data
+     * @return mixed
+     */
     protected function buildEntity($class, $data)
     {
         return with( new $class )->buildNewInstance($data);
     }
 
+    /**
+     * @param null $value
+     * @return DateTime
+     */
     protected function newDateObject($value = null)
     {
         if($value) return new DateTime($value);
         return new DateTime;
     }
 
+    /**
+     * @param $value
+     * @return DateTime
+     */
     protected function newDateFromTimestamp($value)
     {
         $date = new DateTime;
@@ -323,29 +481,54 @@ class AttributeType extends AttributeEnum {
         return $date;
     }
 
+    /**
+     * @param $value
+     * @param $format
+     * @return DateTime
+     */
     protected function newDateFromFormat($value, $format)
     {
         return new DateTime($value);
     }
 
-    private function makeCollection(array $def = null)
+    /**
+     * @param string|null $class
+     * @return object
+     */
+    protected function makeCollection($class = null)
     {
-        if ($def)
+        $class = $class ?: self::DEFAULT_COLLECTION_CLASS;
+        return $this->makeClass($class);
+    }
+
+    /**
+     * @param $class
+     * @param array $args
+     * @return object
+     */
+    protected function makeClass($class, array $args = [])
+    {
+        $classParts = explode(',', $class);
+        $className = array_shift($classParts);
+        $reflection = new \ReflectionClass($className);
+        $methods = [];
+        foreach($classParts as $k => $_arg)
         {
-            $collectionParams = explode('|', $def['collection']);
-            $collClass = array_shift($collectionParams);
-            $collection = new $collClass;
-            foreach($collectionParams as $param)
+            if (str_contains($_arg, ':'))
             {
-                list($pKey, $pVal) = explode(':', $param);
-                $collection->{$pKey}($pVal);
+                $_aparts = explode(':', $_arg);
+                $methods[$_aparts[0]] = $_aparts[1];
+                unset($classParts[$k]);
             }
         }
-        else
+        if ($args || $classParts)
+            $args = array_merge($classParts, $args);
+        $obj = $reflection->newInstance($args);
+
+        foreach($methods as $method => $val)
         {
-            $collClass = self::DEFAULT_COLLECTION_CLASS;
-            $collection = new $collClass;
+            $obj->$method($val);
         }
-        return $collection;
+        return $obj;
     }
 }
