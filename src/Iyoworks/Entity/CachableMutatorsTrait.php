5e7b0fa869afe51da8e64be3fae612809c5a0379
Blessing Osakue <iyoworks@gmail.com>
2015-01-14 14:47:46 -0600

    integrate snakecase options
    Signed-off-by: Blessing Osakue <iyoworks@gmail.com>

--- src/Iyoworks/Entity/CachableMutatorsTrait.php
+++ src/Iyoworks/Entity/CachableMutatorsTrait.php
@@ -18,10 +18,10 @@ trait CachableMutatorsTrait {
     /**
      * @param string $class
      */
-    protected static function cacheMutators($class)
+    protected static function cacheMutators($class, $snakeCaseAttributes = true)
     {
-        $getPattern = 'get(.+)'.static::MUTATOR_SUFFIX;
-        $setPattern = 'set(.+)'.static::MUTATOR_SUFFIX;
+        $getPattern = 'get(.+)' . static::getMutatorSuffix();
+        $setPattern = 'set(.+)' . static::getMutatorSuffix();
         // Here we will extract all of the mutated attributes so that we can quickly
         // spin through them after we export entities to their array form, which we
         // need to be fast. This will let us always know the attributes mutate.
@@ -29,11 +29,13 @@ trait CachableMutatorsTrait {
         {
             if (preg_match("/^{$getPattern}$/", $method, $matches))
             {
-                static::$mutatorCache[$class]['getters'][] = lcfirst($matches[1]);
+                $attr = $snakeCaseAttributes ? snake_case($matches[1]) : lcfirst($matches[1]);
+                static::$mutatorCache[$class]['getters'][$attr] = lcfirst($matches[1]);
             }
             elseif (preg_match("/^{$setPattern}$/", $method, $matches))
             {
-                static::$mutatorCache[$class]['setters'][] = lcfirst($matches[1]);
+                $attr = $snakeCaseAttributes ? snake_case($matches[1]) : lcfirst($matches[1]);
+                static::$mutatorCache[$class]['setters'][$attr] = lcfirst($matches[1]);
             }
         }
     }
@@ -52,16 +54,21 @@ trait CachableMutatorsTrait {
     public function allGetMutators()
     {
         if (isset(static::$mutatorCache[get_class($this)]['getters']))
+        {
             return static::$mutatorCache[get_class($this)]['getters'];
+        }
         return [];
     }
+
     /**
      * @return array
      */
     public function allSetMutators()
     {
         if (isset(static::$mutatorCache[get_class($this)]['setters']))
+        {
             return static::$mutatorCache[get_class($this)]['setters'];
+        }
         return [];
     }
 
@@ -86,21 +93,43 @@ trait CachableMutatorsTrait {
 
     /**
      * Determine if a get mutator exists for an attribute.
-     * @param  string  $key
+     * @param  string $key
      * @return bool
      */
     public function hasGetMutator($key)
     {
-        return method_exists($this, 'get'.studly_case($key).static::MUTATOR_SUFFIX);
+        return method_exists($this, 'get' . studly_case($key) . static::getMutatorSuffix());
     }
 
     /**
      * Determine if a set mutator exists for an attribute.
-     * @param  string  $key
+     * @param  string $key
      * @return bool
      */
     public function hasSetMutator($key)
     {
-        return method_exists($this, 'set'.studly_case($key).static::MUTATOR_SUFFIX);
+        return method_exists($this, 'set' . studly_case($key) . static::getMutatorSuffix());
+    }
+
+    /**
+     * Get the value of an attribute using its mutator.
+     * @param  string $key
+     * @param  mixed $value
+     * @return mixed
+     */
+    protected function mutateAttribute($key, $value)
+    {
+        return $this->{'get' . studly_case($key) . static::getMutatorSuffix()}($value);
+    }
+
+    /**
+     * Get the value of an attribute using its mutator.
+     * @param  string $key
+     * @param  mixed $value
+     * @return mixed
+     */
+    protected function mutateAttributeSetter($key, $value)
+    {
+        return $this->{'set' . studly_case($key) . static::getMutatorSuffix()}($value);
     }
 } 
